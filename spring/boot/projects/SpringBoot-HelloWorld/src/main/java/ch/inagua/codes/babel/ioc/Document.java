4afb3205ec7677059c5494a535e770bb4aa491f8
Jacques COUVREUR <jacques@2ia.net>
2022-10-06 19:12:23 +0200

    feat(SpringBoot/IoC): Refactor Document with introducing interface. #2

--- spring/boot/projects/SpringBoot-HelloWorld/src/main/java/ch/inagua/codes/babel/ioc/Document.java
+++ spring/boot/projects/SpringBoot-HelloWorld/src/main/java/ch/inagua/codes/babel/ioc/Document.java
@@ -1,13 +1,22 @@
 package ch.inagua.codes.babel.ioc;
 
-public class Document {
+interface DocumentInterface {
+    String getPage();
+}
+
+public class Document implements DocumentInterface {
+    @Override
     public String getPage() {
         return "Page!";
     }
 }
 
 class Printer {
-    Document document = new Document();
+    private DocumentInterface document;
+
+    public Printer(DocumentInterface document) {
+        this.document = document;
+    }
 
     public void print() {
         System.out.println(this.document.getPage());
@@ -15,7 +24,8 @@ class Printer {
 }
 
 class Factory {
-    Printer p = new Printer();
+    Document document = new Document();
+    Printer p = new Printer(document);
 
     void produce() {
         p.print();
